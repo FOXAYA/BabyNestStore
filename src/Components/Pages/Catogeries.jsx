a63961cc6844b8a9def7eb21d37956d39fd104c2
FOXAYA <omniasaged111@gmail.com>
2024-12-19 23:43:55 +0200

    CatogriesCard and style it with some css code

--- src/Components/Pages/Catogeries.jsx
+++ src/Components/Pages/Catogeries.jsx
@@ -2,10 +2,15 @@ import React, { Component } from "react";
 
 import Clients from "../Layout/Clients";
 
+import CategoriesList from "../Layout/CatogriesCards";
+
 class Catogries extends Component {
   render() {
     return (
+      <>
       <Clients />
+      <CategoriesList />
+      </>
     );
   }
 }
