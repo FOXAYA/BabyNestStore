8fe46dfa8505c67c93b4ccec40872b29f4eaa892
FOXAYA <omniasaged111@gmail.com>
2024-12-19 23:59:41 +0200

    add responsive to catogries

--- src/Components/Layout/Clients.jsx
+++ src/Components/Layout/Clients.jsx
@@ -1,25 +1,29 @@
 import React, { Component } from "react";
-import img1 from '../assets/images/client-1.webp';
-import img2 from '../assets/images/client-2.webp';
-import img3 from '../assets/images/client-3.webp';
-import img4 from '../assets/images/client-4.webp';
-import img5 from '../assets/images/client-5.webp';
-import img6 from '../assets/images/client-6.webp';
+import { Row, Col, Container } from "react-bootstrap";
+import img1 from "../assets/images/client-1.webp";
+import img2 from "../assets/images/client-2.webp";
+import img3 from "../assets/images/client-3.webp";
+import img4 from "../assets/images/client-4.webp";
+import img5 from "../assets/images/client-5.webp";
+import img6 from "../assets/images/client-6.webp";
 
 class Clients extends Component {
   render() {
     const images = [img1, img2, img3, img4, img5, img6];
     return (
-      <div className="d-flex justify-content-center py-6 clients-images w-100 gap-3 p-5">
-        {images.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt={`Client ${index + 1}`}
-            className="client-img"
-          />
-        ))}
-      </div>
+      <Container className="my-5 h- p-5">
+        <Row className="g-4">
+          {images.map((image, index) => (
+            <Col key={index} xs={6} sm={6} md={3} lg={2}>
+              <img
+                src={image}
+                alt={`Client ${index + 1}`}
+                className="client-img w-100"
+              />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     );
   }
 }
