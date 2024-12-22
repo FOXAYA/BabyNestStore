0f9d3491653440f863ed091fd1812822c2540673
Mariam22MR <xxxsaakk@gmail.com>
2024-12-22 23:38:13 +0300

    test branch

---
+++ src/Components/Layout/mariam.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+class Layou extends Component {
+  render() {
+    return (
+      <>
+            <h1>jjj</h1>
+      </>
+    )
+  }
+}
+export default Layou;
