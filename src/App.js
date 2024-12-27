ff4982f9f3453360a5c2923bc3fddf088cbd2a63
Mariam22MR <xxxsaakk@gmail.com>
2024-12-27 11:32:36 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import Layout from './Components/Layout/Layout';
+import Footer from './Components/Footer/Footer';
 class App extends Component {
   render() {
     return (
       <>
-     <Layout />
+      <Layout />
+      <Footer />
       </>
     )
   }
