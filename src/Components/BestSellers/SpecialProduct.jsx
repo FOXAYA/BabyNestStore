213cee4e93848be4023e1daedd7de735a541cdc5
Mariam22MR <xxxsaakk@gmail.com>
2025-01-07 22:52:27 +0300

    create infrastructure

---
+++ src/Components/BestSellers/SpecialProduct.jsx
@@ -0,0 +1,21 @@
+//import React, { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+//import { Link } from "react-router-dom";
+//import { MdOutlineShoppingCartCheckout } from "react-icons/md";
+//import products from "./ProductsList";
+import product from "../../image/insta-image3.jpg";
+
+const Newcard = () => {
+
+    return (
+        <Container className="my-5">
+        <Row>
+            
+
+    
+        </Row>
+    </Container>
+    );
+};
+
+    export default Newcard;
