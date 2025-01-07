b05c5be76bdc8ca5370f1a71714c982b8b37697f
Mariam22MR <xxxsaakk@gmail.com>
2025-01-07 22:59:57 +0300

    image editing

--- src/Components/BestSellers/SpecialProduct.jsx
+++ src/Components/BestSellers/SpecialProduct.jsx
@@ -15,11 +15,11 @@ const Newcard = () => {
                         src={product} // رابط صورة الطفل
                         alt="Child"
                         className="img-fluid rounded"
-                        
+                        style={{ width: "100%", maxWidth: "600px", borderRadius: "10px" }}
                     />
                 </Col>
 
-                <Col md={6} className="d-flex justify-content-center" style={{background: "#F8F4EC", borderRadius: "10px"}}>
+                <Col md={6} className="d-flex justify-content-center" >
 
 
 
